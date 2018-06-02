09db7f3953d6d6e6cd6b07cb21d36ef66aa45676
Greg Parker <gparker@Gregs-MacBook-Pro.local>
2018-06-02 00:40:11 -0600

    First Commit

---
+++ pythonLED.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+import sys
+from time import sleep
+
+LED4_PATH = "/sys/class/gpio/gpio4/"
+SYSFS_DIR = "/sys/class/gpio"
+LED_NUMBER = "4"
+
+def writeLED (filename, value, path=LED4_PATH):
+	#"This function writes the value passed to the file in the path"
+	fo = open (path + filename, "w")
+	fo.write(value)
+	fo.close()
+	return
+
+print ("Starting the GPIO LED4 Python Script")
+
+if len(sys.argv) !=2:
+	print ("There is an incorrect number of arguments")
+	print ("Usage is: pythonLED.py command")
+	print ("Where command is one of the following:")
+	print ("setup, on, off, status, or close")
+
+	sys.exit(2)
+
+if sys.argv[1] == "on":
+	print ("Turning the LED on")
+	writeLED (filename="value", value="1")
+elif sys.argv[1]=="off":
+	print ("Turning the LED off")
+	writeLED (filename="value", value="0")
+elif sys.argv[1]=="setup":
+	print ("Setting up the LED GPIO")
+	writeLED (filename="export", value=LED_NUMBER, path=SYSFS_DIR)
+	sleep(0.1)
+	writeLED (filename="direction", value="out")
+elif sys.argv[1]=="close":
+	print("Closing down the LED GPIO")
+	writeLED (filename="unexport", value=LED_NUMBER, path=SYSFS_DIR)
+elif sys.argv[1]=="status":
+	print("Getting the LED state value")
+	fo = open(LED4_PATH + "value", "r")
+	print(fo.read())
+	fo.close()
+else:
+	print("Invalid Command: " + sys.argv[1])
+
+print("End of Python Script")
